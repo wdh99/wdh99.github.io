7ab3fd803e4414db274b14c15f9f256cdcedfc63
WuDingHuan <meansvv@163.com>
2022-06-30 02:55:25 +0800

    first commit

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8ebd222607573861aa4217ba654ad12f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
