0c1ab07215eb11b823ca1be286d9a6e93aab3b48
WuDingHuan <meansw@163.com>
2022-07-23 18:29:32 +0800

    更新

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8ebd222607573861aa4217ba654ad12f";
+  hm.src = "https://hm.baidu.com/hm.js?f4356d3c7fab6fec972a2a94b251bea3";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
