e1a4ae57054fbafa6880fd446ae4f097b586109f
WuDingHuan <meansvv@163.com>
2023-04-24 14:07:14 +0800

    更新结构

---
+++ site/tongji/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f4356d3c7fab6fec972a2a94b251bea3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
